c64e2e758f4191de7cd896525613250f24aa13be
icedevil777111 <icedevil777111@mail.ru>
2021-11-16 17:09:23 +0300

    add base

--- app/bboard/views.py
+++ app/bboard/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Bb
+
+def index(request):
+    s = 'Список объявлений\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title +'\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
