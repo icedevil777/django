43db9c95858fb6793c4108ead5f03339fa5eea28
icedevil777111 <icedevil777111@mail.ru>
2021-11-17 16:52:23 +0300

    add html

--- app/bboard/admin.py
+++ app/bboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bb, Rubric
+
+class Bbadmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published', 'rubric')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+admin.site.register(Bb, Bbadmin)
+
+admin.site.register(Rubric)
--- app/bboard/views.py
+++ app/bboard/views.py
@@ -1,9 +1,29 @@
 from django.http import HttpResponse
-
-from .models import Bb
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Bb, Rubric
+from .forms import BbForm
 
 def index(request):
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title +'\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
